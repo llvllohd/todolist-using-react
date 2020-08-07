b86bc68b20d59fca05c46c960e993db28a9463e9
Asif Ahmed <llvllohd@gmail.com>
2020-08-07 17:51:24 +0530

    Updates

---
+++ precache-manifest.f5b587c87566aaa87e5307102300390b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d54047a6dbd9168cacfd565de46c928",
+    "url": "/todolist-using-react/index.html"
+  },
+  {
+    "revision": "b0e98344c9afc6403aed",
+    "url": "/todolist-using-react/static/css/main.f485fdee.chunk.css"
+  },
+  {
+    "revision": "00c6ceea8483c88adae7",
+    "url": "/todolist-using-react/static/js/2.db7fedbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-using-react/static/js/2.db7fedbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e98344c9afc6403aed",
+    "url": "/todolist-using-react/static/js/main.9d55fe18.chunk.js"
+  },
+  {
+    "revision": "cee5dea7dd0fda0698cc",
+    "url": "/todolist-using-react/static/js/runtime-main.04b6604c.js"
+  }
+]);
